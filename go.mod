c4bf37e3c70e85f77791242f7b6c24f62cfaeb64
rizama <rizkysampratama@gmail.com>
2023-02-04 17:50:39 +0700

    upgrade dependency

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/rizama/app-go-sayhello
 
 go 1.19
 
-require github.com/rizama/go-lib-hello v1.0.0 // indirect
+require github.com/rizama/go-lib-hello/v2 v2.0.0-20230112063037-211b64803901 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/rizama/go-lib-hello"
+	say_hello "github.com/rizama/go-lib-hello/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(say_hello.SayHello("sam"))
 }
